8db8ead3322f00dc23116aedd3f37a4899b5c91d
赵传保 <zhaochuanbao@huiwenliye.com>
2017-12-05 17:45:55 +0800

    Initial Commit

---
+++ AVPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AVPlayer
+//
+//  Created by 赵传保 on 2017/12/5.
+//  Copyright © 2017年 赵传保. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AVPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AVPlayer
+//
+//  Created by 赵传保 on 2017/12/5.
+//  Copyright © 2017年 赵传保. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
